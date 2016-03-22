22ca07718e1556a3ac95a809998f240a67827a9d
gkanteres <gregkanteres@gmail.com>
2016-03-22 10:48:13 -0700

    Add category dropdown

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
    root 'categories#index'
+
    #devise_for :users, :controllers => { registrations: 'registrations'}
+
    resources :categories do
      resources :products
    end

fedaa5eec93d4b42a4ec17b0f0107525803a4381
Gregory K <gregkanteres@gmail.com>
2016-03-24 09:37:21 -0700

    Update specs

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :review_title }
+  it { should validate_presence_of :review_content }
+  it { should validate_presence_of :rating }
+  it { should belong_to :user }
+  it { should belong_to :product }
 end

14a31202e677ac19575a4801898da7efd385986d
Gregory K <gregkanteres@gmail.com>
2016-03-30 09:47:07 -0700

    Update specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe Product, type: :model do
   it { should belong_to :category }
   it { should belong_to :user }
   it { should belong_to :cart }
-  it { should have_and_belong_to_many :tags }
+  it { should have_many :tags }
+  it { should have_many :product_tags }
   it { should validate_presence_of :name }
   it { should validate_presence_of :description }
   it { should validate_presence_of :price }
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  it { should have_and_belong_to_many :products }
+  it { should have_many :products }
+  it { should have_many :product_tags}
   it { should validate_presence_of :name }
 
 end

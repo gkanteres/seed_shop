89a72ca1ce13a809f2b1725958fb03a64aa0c486
gkanteres <gregkanteres@gmail.com>
2016-04-03 13:19:07 -0700

    Add db:seeds:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+Category.delete_all
+Category.create! id: 1, name: "Wildflowers", description: "Low-maintenance annuals and perennials"
+Category.create! id: 2, name: "Culinary Herbs", description: "Herbs for the kitchen"
+Category.create! id: 3, name: "Heirloom Vegetables", description: "Heirloom varieties of all your favorite garden vegetables"
+
 Product.delete_all
-Product.create! id: 1, category_id: 5, name: "Balsamroot", latin_name: "Balsamorhiza sagittata", description: "Small sunflower relative", price: 3.00, image: "/assets/balsamroot.jpg", active: true
-Product.create! id: 2, category_id: 5, name: "Broad-Leaf Lupine", latin_name: "Lupinus latifolius", description: "Purple-blue lupine of Western US", price: 4.00, image: "/assets/lupine.jpg", active: true
-Product.create! id: 3, category_id: 5, name: "Western Columbine", latin_name: "Aquilegia formosa", description: "Western native woodland wildflower", price: 5.00, image: "/assets/Western_Columbine.jpg", active: true
+Product.create! id: 1, category_id: 1, name: "Balsamroot", latin_name: "Balsamorhiza sagittata", description: "Small sunflower relative", price: 3.00, image: "/assets/balsamroot.jpg", active: true
+Product.create! id: 2, category_id: 1, name: "Broad-Leaf Lupine", latin_name: "Lupinus latifolius", description: "Purple-blue lupine of Western US", price: 4.00, image: "/assets/lupine.jpg", active: true
+Product.create! id: 3, category_id: 1, name: "Western Columbine", latin_name: "Aquilegia formosa", description: "Western native woodland wildflower", price: 5.00, image: "/assets/Western_Columbine.jpg", active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"

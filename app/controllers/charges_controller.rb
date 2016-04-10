61f75c34218605df82e09caeb3b37ef1191c90cc
gkanteres <gregkanteres@gmail.com>
2016-04-10 09:42:38 -0700

    Update charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,2 +1,25 @@
 class ChargesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    #Amount in cents
+    amount = params[:stripeAmount].to_i * 100
+
+    #Create the customer in Stripe
+    customer = Stripe::Customer.create(
+    email: params[:stripeEmail],
+    card: params[:stripeToken]
+    )
+
+    #Create the charge using the customer data returned by Stripe API
+    charge = Stripe::Charge.create(
+    customer: customer.id,
+    amount: amount,
+    description: 'Rails Stripe customer',
+    currency: 'usd'
+    )
+  end
+
 end
